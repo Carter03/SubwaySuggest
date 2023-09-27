28351e9e1340626eacdba445d4e39b7cd70f9aa8
Carter Timmons <nebrichc@gmail.com>
2023-09-27 13:50:54 -0400

    Added utils, restructured more

--- src/testmain.py
+++ src/testmain.py
@@ -1,9 +1,11 @@
 import datamanager
 import sandwichDNN
+from utils import guiLaunch
+
+guiLaunch.OpenWeb()
 
 dm = datamanager.DataManager()
 model = sandwichDNN.Model(dm)
 
 model.Fit()
-
 print(model.Predict([4, 1], 3))
--- src/utils/guiLaunch.py
+++ src/utils/guiLaunch.py
@@ -1,6 +1,5 @@
-
-
 from flask import Flask, render_template
+import webbrowser
 
 app = Flask(__name__)
 
@@ -8,5 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def OpenWeb():
+    webbrowser.open('http://127.0.0.1:5000', new=1)
+    app.run(host='127.0.0.1')
