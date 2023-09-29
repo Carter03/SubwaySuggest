b67ff4d4e4ce8994e317cf622ae5da74ef7ba61f
Carter Timmons <nebrichc@gmail.com>
2023-09-29 13:37:33 -0400

    none

--- src/htmledit.py
+++ src/htmledit.py
@@ -1,5 +1,20 @@
 from bs4 import BeautifulSoup as bs
 
+class Editor():
+    def __init__(self, htmlpath, subpath):
+        self.indexPath = htmlpath
+        self.subpath = subpath
+        self.soup = bs(open(indexPath), 'html.parser')
+    
+    def ReplaceSubs(data): # list[4] of [sub num, sub desc, sub cost]
+        subdata = data.copy()
+        for idx, sub in enumerate(subdata):
+            subImg = soup.find(attrs={'id':f'sub{idx+1}'})
+            sub['src'] = r'../static/images/sub1.jpeg'
+
+
+        
+
 indexPath = 'utils/templates/index.html'
 
 soup = bs(open(indexPath), 'html.parser')
